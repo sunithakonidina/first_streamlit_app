8e9017b257dc350b9c8b8f69fecfa29f79d5c6ee
sunithakonidina <133619283+sunithakonidina@users.noreply.github.com>
2023-05-16 08:21:18 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.title('my parents new healthy diner')
 streamlit.header('break fast menu')
 streamlit.text('omega 3 &blueberry oatmeal')
 streamlit.text('kale,spinach & rocket smoothie')
-streamit.text('hard-boiled free range egg')
+streamlit.text('hard-boiled free range egg')
 
 
 
