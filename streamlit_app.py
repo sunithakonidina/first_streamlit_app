94b17471c153a380cd369b315561cd07b36fc500
sunithakonidina <133619283+sunithakonidina@users.noreply.github.com>
2023-05-16 09:13:07 +0530

    Update streamlit_app.py
    added toast and avacado

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,11 @@ import streamlit
 
 streamlit.title('my parents new healthy diner')
 
-streamlit.header('break fast menu')
-streamlit.text('omega 3 &blueberry oatmeal')
-streamlit.text('kale,spinach & rocket smoothie')
-streamlit.text('hard-boiled free-range egg')
+streamlit.header('Break-Fast favorites')
+streamlit.text('🥣 omega 3 &blueberry oatmeal')
+streamlit.text('🥗 kale,spinach & rocket smoothie')
+streamlit.text('🐔 hard-boiled free-range egg')
+streamlit.text('🥑🍞 Avocado Tost')
 
 
-
+   
