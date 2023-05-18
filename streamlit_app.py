f7da0d6c5d1b28b0e5329d5db8b8762fed99efab
sunithakonidina <133619283+sunithakonidina@users.noreply.github.com>
2023-05-18 14:28:41 +0530

    Update streamlit_app.py
    frutyciedisplay

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)
 
 streamlit.dataframe(fruits_to_show)
+# new section to display fruityvice api response
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
-import snowflake.connector
